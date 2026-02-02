4f80133497b636a647ad770137b8789300d966c3
Eli Morgan <eliwmorgan@gmail.com>
2026-02-02 16:47:39 +0000

    Merge main and add Doubles page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import TagsPage from "./pages/TagsPage";
 import PuttingPage from "./pages/PuttingPage";
-import DoublesPage from "./pages/DoublesPage"; // ✅ matches your filename
+import DoublesPage from "./pages/DoublesPage";
 
 export default function App() {
   return (
@@ -13,7 +13,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/tags" element={<TagsPage />} />
         <Route path="/putting" element={<PuttingPage />} />
-        <Route path="/doubles" element={<DoublesPage />} /> {/* ✅ NEW */}
+        <Route path="/doubles" element={<DoublesPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
