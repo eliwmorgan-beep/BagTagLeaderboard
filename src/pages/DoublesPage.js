ebfd8c07b9962836b8c73c0c7360e9c054ac8957
Eli Morgan <eliwmorgan@gmail.com>
2026-02-02 18:39:54 +0000

    Redesign

--- src/pages/DoublesPage.js
+++ src/pages/DoublesPage.js
@@ -140,7 +140,6 @@ export default function DoublesPage() {
       () => setLoading(false)
     );
     return () => unsub();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [leagueRef, defaultDoubles]);
 
   const isSeated = (doubles?.format || "random") === "seated";
