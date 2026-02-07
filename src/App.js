a3fa34119d707a82f92322dbe9652690dec4dde7
Eli Morgan <eliwmorgan@gmail.com>
2026-02-07 03:56:32 +0000

    clone page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
+// src/App.js
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import HomePage from "./pages/HomePage";
-import LeaguePage from "./pages/LeaguePage";
 import TagsPage from "./pages/TagsPage";
 import PuttingPage from "./pages/PuttingPage";
 import DoublesPage from "./pages/DoublesPage";
+import CloneLeaguePage from "./pages/CloneLeaguePage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* League chooser */}
-        <Route path="/" element={<HomePage />} />
+        {/* Home: send to your primary league route */}
+        <Route
+          path="/"
+          element={<Navigate to="/league/default-league" replace />}
+        />
 
-        {/* League hub */}
-        <Route path="/league/:leagueId" element={<LeaguePage />} />
-
-        {/* League-specific sections */}
-        <Route path="/league/:leagueId/tags" element={<TagsPage />} />
+        {/* Per-league pages */}
+        <Route path="/league/:leagueId" element={<TagsPage />} />
         <Route path="/league/:leagueId/putting" element={<PuttingPage />} />
         <Route path="/league/:leagueId/doubles" element={<DoublesPage />} />
 
-        {/* Old routes -> send home */}
-        <Route path="/tags" element={<Navigate to="/" replace />} />
-        <Route path="/putting" element={<Navigate to="/" replace />} />
-        <Route path="/doubles" element={<Navigate to="/" replace />} />
-        <Route path="/league" element={<Navigate to="/" replace />} />
+        {/* Clone League utility page */}
+        <Route path="/clone-league" element={<CloneLeaguePage />} />
 
-        {/* Catch-all */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        {/* Fallback */}
+        <Route
+          path="*"
+          element={<Navigate to="/league/default-league" replace />}
+        />
       </Routes>
     </BrowserRouter>
   );
