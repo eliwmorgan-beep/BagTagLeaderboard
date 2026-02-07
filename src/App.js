68d8dff78dafa00defedabfddabc7fae7c19aa0b
Eli Morgan <eliwmorgan@gmail.com>
2026-02-07 01:48:48 +0000

    adding league page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import TagsPage from "./pages/TagsPage";
 import PuttingPage from "./pages/PuttingPage";
 import DoublesPage from "./pages/DoublesPage";
+import LeaguePage from "./pages/LeaguePage";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/tags" element={<TagsPage />} />
         <Route path="/putting" element={<PuttingPage />} />
         <Route path="/doubles" element={<DoublesPage />} />
+        <Route path="/league" element={<LeaguePage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/LeaguePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+export default function LeaguePage() {
+  return (
+    <div>
+      <Header />
+      <div style={{ padding: 16 }}>
+        <h1>League</h1>
+        <p>This is the League page.</p>
+      </div>
+    </div>
+  );
+}
